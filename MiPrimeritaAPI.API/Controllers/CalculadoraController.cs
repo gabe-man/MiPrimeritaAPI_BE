3396b01395ed09a9fd0bd838ff851b15f40d9616
Jose <Jose@DESKTOP-U26FL81>
2022-05-09 14:18:01 +0200

    Agregar archivos de proyecto.

---
+++ MiPrimeritaAPI.API/Controllers/CalculadoraController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiPrimeritaAPI.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CalculadoraController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Suma")]
+        public int Suma(int n1, int n2)
+        {
+           return n1 + n2;
+        }
+
+        [HttpGet]
+        [Route("Division")]
+        public int Division(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+    }
+}
---
+++ MiPrimeritaAPI.DAL/IESContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MiPrimeritaAPI.DAL.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimeritaAPI.DAL
+{
+    public class IESContext : DbContext
+    {
+        public DbSet<Alumno> Alumnos { get; set; }
+        protected readonly IConfiguration Configuration;
+
+        public IESContext() {}
+
+        public IESContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+            //var connectionString = Configuration.GetConnectionString("ConexionAPidgey");
+            options.UseMySql("server=localhost;port=3306;database=ies;user=pidgey;password=P@ssw0rd;", 
+                ServerVersion.AutoDetect("server=localhost;port=3306;database=ies;user=pidgey;password=P@ssw0rd;"));
+        }
+    }
+}
